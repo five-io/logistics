cfb62c5702b298ec5b3235344e807fe071a10ab6
khloe08 <owlnae04@gmail.com>
2025-03-15 18:28:35 +0900

    [feat #17] Common 모듈 및 Auditing 설정(날짜만)

--- hub/src/main/java/com/msa/fiveio/hub/HubApplication.java
+++ hub/src/main/java/com/msa/fiveio/hub/HubApplication.java
@@ -1,10 +1,14 @@
 package com.msa.fiveio.hub;
 
+
+import com.msa.fiveio.common.config.JpaAuditingConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
 @EnableFeignClients
+@Import(JpaAuditingConfig.class)
 @SpringBootApplication
 public class HubApplication {
 

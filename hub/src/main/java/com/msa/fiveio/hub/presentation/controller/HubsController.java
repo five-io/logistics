1b50663305d20c4240162b035ad09bc24c700064
khloe08 <owlnae04@gmail.com>
2025-03-16 20:33:34 +0900

    [feat #22] Swagger 설정

--- hub/src/main/java/com/msa/fiveio/hub/presentation/controller/HubsController.java
+++ hub/src/main/java/com/msa/fiveio/hub/presentation/controller/HubsController.java
@@ -6,11 +6,14 @@ import com.msa.fiveio.hub.application.facade.HubsFacade;
 import com.msa.fiveio.hub.presentation.dto.HubsRequestDto;
 import com.msa.fiveio.hub.presentation.dto.HubsResponseDto;
 import com.msa.fiveio.hub.presentation.dto.SearchResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,29 +26,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/hubs")
 @RequiredArgsConstructor
+@Tag(name = "Hub Service", description = "허브 서비스 API")
 public class HubsController {
 
     private final HubsFacade hubsFacade;
 
+    @Operation(summary = "Hub 등록", description = "Hub 등록 api 입니다.")
     @PostMapping("/create")
     public ResponseEntity<HubsResponseDto> createHubs(@RequestBody HubsRequestDto hubsDto) {
         return ResponseEntity.ok(hubsFacade.createHubs(hubsDto));
     }
 
+    @Operation(summary = "Hub 단 건 조회", description = "Hub 단 건 조회 api 입니다.")
     @GetMapping("/read")
     public ResponseEntity<HubsResponseDto> readHubs(@RequestParam UUID id) {
         return ResponseEntity.ok(hubsFacade.readHubs(id));
     }
 
+    @Operation(summary = "Hub 수정", description = "Hub 수정 api 입니다.")
     @PatchMapping("/update/{id}")
     public ResponseEntity<HubsResponseDto> updateHubs(@PathVariable UUID id, @RequestBody HubsRequestDto hubsDto) {
         return ResponseEntity.ok(hubsFacade.updateHubs(id,hubsDto));
     }
 
+    @Operation(summary = "Hub Search", description = "Hub Search api 입니다.")
     @GetMapping("/search")
     public ResponseEntity<Page<SearchResponseDto>> searchAddress(
         HubsRequestDto hubsDto, Pageable pageable) {
         return ResponseEntity.ok(hubsFacade.searchHubs(hubsDto,pageable));
+    }
+
+    @Operation(summary = "Hub 삭제", description = "Hub 삭제 api 입니다.")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteHubs(@PathVariable UUID id) {
+        hubsFacade.deleteHubs(id);
+        return ResponseEntity.noContent().build();
     }
 
 }

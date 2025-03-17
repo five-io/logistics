9f183b0f9dd38190ad0110375462cc81626e86e2
문고은 <>
2025-03-17 23:55:50 +0900

    [feat #31] DeliveryManagers 클래스 생성, 배송 담당자 컬럼명 변경

---
+++ delivery/src/main/java/com/msa/fiveio/delivery/infrastructure/client/dto/DeliveryManagers.java
@@ -0,0 +1,15 @@
+package com.msa.fiveio.delivery.infrastructure.client.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DeliveryManagers {
+
+    private Long companyDeliveryManager;
+    private Long hubDeliveryManager;
+
+}

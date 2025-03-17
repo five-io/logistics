c284cc39255770c5f240c8ea5609576ec64edad9
syn <tlsdpsk1992@naver.c0m>
2025-03-17 12:25:28 +0900

    [feat #28] 업체단건조회 API 생성, dto이름변경

--- company/src/main/java/com/msa/fiveio/company/application/usecase/CompanyService.java
+++ company/src/main/java/com/msa/fiveio/company/application/usecase/CompanyService.java
@@ -1,10 +1,19 @@
 package com.msa.fiveio.company.application.usecase;
 
+import com.msa.fiveio.company.presentation.dto.CompanyCreateRequestDto;
+import com.msa.fiveio.company.presentation.dto.CompanyGetResponseDto;
 import com.msa.fiveio.company.presentation.dto.CompanyRequestDto;
-import com.msa.fiveio.company.presentation.dto.CompanyResponseDto;
+import com.msa.fiveio.company.presentation.dto.CompanyCreateResponseDto;
+
+import java.util.UUID;
 
 public interface CompanyService {
 
     //업체생성
-    CompanyResponseDto createCompanys(CompanyRequestDto requestDto);
+    CompanyCreateResponseDto createCompanys(CompanyCreateRequestDto requestDto);
+
+    //업체단건조회
+    CompanyGetResponseDto getCompanys(UUID companyId);
+
+
 }

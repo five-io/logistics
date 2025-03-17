bac0256b7a3ad071ef473b3eac62fddfc96b2655
syn <tlsdpsk1992@naver.c0m>
2025-03-17 16:44:24 +0900

    [feat #30] Mapper 생성, 적용

--- company/src/main/java/com/msa/fiveio/company/application/usecase/CompanyServiceImpl.java
+++ company/src/main/java/com/msa/fiveio/company/application/usecase/CompanyServiceImpl.java
@@ -3,13 +3,11 @@ package com.msa.fiveio.company.application.usecase;
 import com.msa.fiveio.common.exception.CustomException;
 import com.msa.fiveio.common.exception.domain.CompanysErrorCode;
 import com.msa.fiveio.company.model.entity.Companys;
+import com.msa.fiveio.company.model.entity.CompanysType;
 import com.msa.fiveio.company.model.repository.CompanysRepository;
-import com.msa.fiveio.company.presentation.dto.request.CompanyCreateRequestDto;
-import com.msa.fiveio.company.presentation.dto.request.CompanyUpdateRequestDto;
-import com.msa.fiveio.company.presentation.dto.response.CompanyCreateResponseDto;
-import com.msa.fiveio.company.presentation.dto.response.CompanyGetResponseDto;
-import com.msa.fiveio.company.presentation.dto.response.CompanyUpdateResponseDto;
-import com.msa.fiveio.company.presentation.mapper.CompanysMapper;
+import com.msa.fiveio.company.presentation.dto.CompanyCreateRequestDto;
+import com.msa.fiveio.company.presentation.dto.CompanyCreateResponseDto;
+import com.msa.fiveio.company.presentation.dto.CompanyGetResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,11 +25,16 @@ public class CompanyServiceImpl implements CompanyService {
     @Override
     public CompanyCreateResponseDto createCompanys(CompanyCreateRequestDto requestDto) {
         //requestdto -> 엔티티 변환
-        Companys company = CompanysMapper.CreateRequestDtoToEntity(requestDto);
+        Companys company = Companys.builder()
+                .companyName(requestDto.getCompanyName())
+                .address(requestDto.getCompanyAddress())
+                .companyTypes((CompanysType) requestDto.getCompanyType())
+                .hubID(requestDto.getHubId())
+                .build();
         //엔티티 저장
         companysRepository.save(company);
         //dto 반환
-        return CompanysMapper.entityToCreateCompanysResponseDto(company);
+        return CompanyCreateResponseDto.of(company);
     }
 
     public CompanyGetResponseDto getCompanys(UUID companyId) {
@@ -39,28 +42,6 @@ public class CompanyServiceImpl implements CompanyService {
         Companys company = companysRepository.findById(companyId).orElseThrow(
                 () -> new CustomException(CompanysErrorCode.COMPANYS_NOT_FOUND));
 
-        return CompanysMapper.entityToGetCompanyResponseDto(company);
-    }
-
-    @Override
-    public void deleteCompany(UUID companyId) {
-        //id로 리포지토리에서 찾기
-        Companys company = companysRepository.findById(companyId).orElseThrow(
-                () -> new CustomException(CompanysErrorCode.COMPANYS_NOT_FOUND));
-        //삭제
-        companysRepository.deleteById(companyId);
-    }
-
-    @Override
-    public CompanyUpdateResponseDto updateCompany(UUID companyId, CompanyUpdateRequestDto requestDto) {
-        //id로 데이터에 저장되어 있는지 확인
-        Companys company =companysRepository.findById(companyId).orElseThrow(
-                () -> new CustomException(CompanysErrorCode.COMPANYS_NOT_FOUND));
-        //DTO 받아온걸로 업데이트
-        company.update(requestDto);
-        //업데이트한 엔티티로 save
-        companysRepository.save(company);
-        return CompanysMapper.entityToUpdateCompanyResponseDto(company);
-
+        return CompanyGetResponseDto.of(company);
     }
 }

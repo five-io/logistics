f24acd51e0efea9cbb0a3444fadd2ea77139c68b
khloe08 <owlnae04@gmail.com>
2025-03-18 11:04:50 +0900

    [feat #35] Gateway Exception

--- gateway/src/main/java/com/spring_cloud/client/gateway/exception/AccessTokenExpiredException.java
+++
@@ -1,8 +0,0 @@
-package com.spring_cloud.client.gateway.exception;
-
-public class AccessTokenExpiredException extends BaseException {
-  public AccessTokenExpiredException() {
-    super(702, "Access token has expired.");
-  }
-}
-
--- gateway/src/main/java/com/spring_cloud/client/gateway/exception/NotExistsAuthorization.java
+++
@@ -1,7 +0,0 @@
-package com.spring_cloud.client.gateway.exception;
-
-public class NotExistsAuthorization extends BaseException {
-  public NotExistsAuthorization() {
-    super(701, "Authorization header is missing or invalid.");
-  }
-}

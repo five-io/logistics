f4254d52a13d46cb080e41a25282be45c6ae614c
문고은 <>
2025-03-18 19:52:59 +0900

    [feat #46] Order 검색을 위한 커스텀 Repository 생성

--- order/src/main/java/com/msa/fiveio/order/infrastructure/repository/JpaOrderRepository.java
+++ order/src/main/java/com/msa/fiveio/order/infrastructure/repository/JpaOrderRepository.java
@@ -5,6 +5,6 @@ import com.msa.fiveio.order.model.repository.OrderRepository;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID> {
+public interface JpaOrderRepository extends OrderRepository, JpaRepository<Order, UUID>, JpaOrderRepositoryCustom {
 
 }

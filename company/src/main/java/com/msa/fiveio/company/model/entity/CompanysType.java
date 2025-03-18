cd44d26aa2ae3ecc2de0b9e25d695d4eb448f419
syn <tlsdpsk1992@naver.c0m>
2025-03-19 00:35:52 +0900

    [feat #39] enum타입 value 괄호추가

--- company/src/main/java/com/msa/fiveio/company/model/entity/CompanysType.java
+++ company/src/main/java/com/msa/fiveio/company/model/entity/CompanysType.java
@@ -1,6 +1,16 @@
 package com.msa.fiveio.company.model.entity;
 
 public enum CompanysType {
-    MANUFACTURER,  //생산업체
-    RECEIVER  //수령업체
+    MANUFACTURER("생산업체"),
+    RECEIVER("수령업체");
+
+    private final String value;
+
+    CompanysType(String value) {
+        this.value = value;
+    }
+
+    public String getScript() {
+        return value;
+    }
 }

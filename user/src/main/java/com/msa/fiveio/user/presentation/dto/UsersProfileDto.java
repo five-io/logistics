fab742416d3e3b0d71f58e278e291cea39165118
Taehoon <lth2943@naver.com>
2025-03-19 00:28:55 +0900

    불필요한 코드 수정

--- user/src/main/java/com/msa/fiveio/user/presentation/dto/UsersProfileDto.java
+++ user/src/main/java/com/msa/fiveio/user/presentation/dto/UsersProfileDto.java
@@ -1,25 +1,17 @@
 package com.msa.fiveio.user.presentation.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
+import lombok.Getter;
 
-@Builder
 @Getter
-@NoArgsConstructor(force = true)
-@AllArgsConstructor
 public class UsersProfileDto {
 
-    private final String username;
+  private String username;
 
-    private final String name;
+  private String name;
 
-    private final String email;
+  private String email;
 
-    private final LocalDateTime createDate;
+  private LocalDateTime createDate;
 
 }

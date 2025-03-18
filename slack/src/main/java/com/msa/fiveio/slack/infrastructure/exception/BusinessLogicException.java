0589b267d91042c993c4ddd169233a19b8f75f21
adgjl <adgjl0122@naver.com>
2025-03-18 17:56:31 +0900

    [feat #47] slacks errorcode 통합으로 제거

--- slack/src/main/java/com/msa/fiveio/slack/infrastructure/exception/BusinessLogicException.java
+++
@@ -1,7 +0,0 @@
-package com.msa.fiveio.slack.infrastructure.exception;
-
-public class BusinessLogicException extends RuntimeException {
-	public BusinessLogicException(String message) {
-		super(message);
-	}
-}

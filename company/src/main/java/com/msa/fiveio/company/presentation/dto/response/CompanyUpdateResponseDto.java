313220ed75f619257c8ada35da5129069c393961
syn <tlsdpsk1992@naver.c0m>
2025-03-18 14:14:01 +0900

    [feat #39] update API 생성

--- company/src/main/java/com/msa/fiveio/company/presentation/dto/response/CompanyUpdateResponseDto.java
+++ company/src/main/java/com/msa/fiveio/company/presentation/dto/response/CompanyUpdateResponseDto.java
@@ -1,16 +1,18 @@
-package com.msa.fiveio.company.presentation.dto;
+package com.msa.fiveio.company.presentation.dto.response;
 
 import com.msa.fiveio.company.model.entity.CompanysType;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.util.UUID;
 
 @Getter
-public class CompanyCreateRequestDto {
-
+@Builder
+public class CompanyUpdateResponseDto {
+    private UUID companyId;
     private CompanysType companyType;
+    private String companyName;
     private String companyAddress;
     private UUID hubId;
-    private String companyName;
 
 }

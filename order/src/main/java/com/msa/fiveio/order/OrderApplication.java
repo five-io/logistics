949c432b911ef361e2694ffc1e99300045481a24
문고은 <>
2025-03-18 20:32:23 +0900

    [feat #46] QueryDsl, JpaAuditing Config 임포트

--- order/src/main/java/com/msa/fiveio/order/OrderApplication.java
+++ order/src/main/java/com/msa/fiveio/order/OrderApplication.java
@@ -1,9 +1,13 @@
 package com.msa.fiveio.order;
 
+import com.msa.fiveio.common.config.JpaAuditingConfig;
+import com.msa.fiveio.common.config.QueryDslConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
 
+@Import({JpaAuditingConfig.class, QueryDslConfig.class})
 @EnableFeignClients
 @SpringBootApplication
 public class OrderApplication {

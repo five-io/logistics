1ace45d0f7860014de75f609e77ab67f3847ffa7
문고은 <>
2025-03-19 18:55:29 +0900

    [feat #71] 배송 상태 설명, 상태 목록 반환 메서드 추가

--- delivery/src/main/java/com/msa/fiveio/delivery/model/entity/enums/DeliveryStatus.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/model/entity/enums/DeliveryStatus.java
@@ -1,10 +1,26 @@
 package com.msa.fiveio.delivery.model.entity.enums;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum DeliveryStatus {
-    HUB_PENDING,                // 허브 대기중
-    IN_TRANSIT_TO_HUB,          // 허브 이동중
-    ARRIVED_AT_DESTINATION_HUB, // 목적지 허브 도착
-    COMPANY_TRANSIT,            // 업체 이동중
-    OUT_FOR_DELIVERY,           // 배송중
-    DELIVERED                   // 배송완료
+    HUB_PENDING("허브 대기중"),
+    IN_TRANSIT_TO_HUB("허브 이동중"),
+    ARRIVED_AT_DESTINATION_HUB("목적지 허브 도착"),
+    COMPANY_TRANSIT("업체 이동중"),
+    OUT_FOR_DELIVERY("배송중"),
+    DELIVERED("배송완료");
+
+    private final String description;
+
+    public static List<String> getAllowedStatuses() {
+        return Arrays.stream(values())
+            .map(Enum::name)
+            .collect(Collectors.toList());
+    }
 }

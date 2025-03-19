1dd014a952bc17d5acc2cd7b934c634af42837c6
문고은 <>
2025-03-19 18:58:11 +0900

    [feat #71] 배송 상태 값 업데이트 구현

--- delivery/src/main/java/com/msa/fiveio/delivery/application/facade/DeliveryFacade.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/application/facade/DeliveryFacade.java
@@ -1,9 +1,11 @@
 package com.msa.fiveio.delivery.application.facade;
 
 import com.msa.fiveio.delivery.presentation.dto.request.DeliveryCreateRequestDto;
+import java.util.UUID;
 
 public interface DeliveryFacade {
 
     void createDelivery(DeliveryCreateRequestDto deliveryRequestDto);
 
+    String updateStatus(UUID deliveryId, String status);
 }
--- delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/DeliveryService.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/DeliveryService.java
@@ -1,9 +1,11 @@
 package com.msa.fiveio.delivery.application.usecase;
 
 import com.msa.fiveio.delivery.presentation.dto.request.DeliveryCreateRequestDto;
+import java.util.UUID;
 
 public interface DeliveryService {
 
     void createDelivery(DeliveryCreateRequestDto deliveryCreateRequestDto);
 
+    String updateStatus(UUID deliveryId, String status);
 }

d1c4e25b0408c0c12e35858fa995a2c5aadfd87d
문고은 <>
2025-03-20 00:41:05 +0900

    [feat #84] OrderServiceImpl에서 외부 서비스 호출 분리

--- order/src/main/java/com/msa/fiveio/order/application/usecase/OrderService.java
+++ order/src/main/java/com/msa/fiveio/order/application/usecase/OrderService.java
@@ -1,8 +1,8 @@
 package com.msa.fiveio.order.application.usecase;
 
-import com.msa.fiveio.order.presentation.dto.request.OrderCreateRequestDto;
+import com.msa.fiveio.order.infrastructure.client.dto.response.CompanyResponseDto;
+import com.msa.fiveio.order.model.entity.Order;
 import com.msa.fiveio.order.presentation.dto.request.OrderSearchRequestDto;
-import com.msa.fiveio.order.presentation.dto.response.OrderCreateResponseDto;
 import com.msa.fiveio.order.presentation.dto.response.OrderResponseDto;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Pageable;
 
 public interface OrderService {
 
-    OrderCreateResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto);
+    Order createOrder(CompanyResponseDto companyInfo, Order order);
 
     Page<OrderResponseDto> readOrders(OrderSearchRequestDto requestDto, Pageable pageable);
 

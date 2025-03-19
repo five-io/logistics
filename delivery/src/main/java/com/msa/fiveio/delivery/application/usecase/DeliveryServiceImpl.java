5a499e466128833cc3f07a49fa2881d3ab2eaf38
문고은 <>
2025-03-19 17:05:18 +0900

    [feat #63] RabbitMQ 관련 로직 제거 및 배송 생성 API 추가

--- delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/DeliveryServiceImpl.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/DeliveryServiceImpl.java
@@ -1,8 +1,6 @@
 package com.msa.fiveio.delivery.application.usecase;
 
 import com.msa.fiveio.delivery.infrastructure.client.dto.DeliveryManagers;
-import com.msa.fiveio.delivery.presentation.mapper.DeliveryMapper;
-import com.msa.fiveio.delivery.infrastructure.messaging.DeliveryCreatedCompletedMessage;
 import com.msa.fiveio.delivery.model.entity.Delivery;
 import com.msa.fiveio.delivery.model.entity.DeliveryFactory;
 import com.msa.fiveio.delivery.model.repository.DeliveryRepository;
@@ -10,8 +8,6 @@ import com.msa.fiveio.delivery.presentation.dto.request.DeliveryCreateRequestDto
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -20,15 +16,10 @@ import org.springframework.stereotype.Service;
 public class DeliveryServiceImpl implements DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
-    private final RabbitTemplate rabbitTemplate;
-
-    @Value("${message.delivery.queue.created.completed}")
-    private String queueCreatedCompleted;
 
     @Override
     public void createDelivery(
         DeliveryCreateRequestDto deliveryCreateRequestDto) {
-
 //        DeliveryManagers managers = requestDeliveryManagerConfirmation(
 //            deliveryCreateRequestDto.getDepartHubId(),
 //            deliveryCreateRequestDto.getArriveHubId()
@@ -47,7 +38,6 @@ public class DeliveryServiceImpl implements DeliveryService {
         log.info("배송 생성 완료, 주문 ID: {}, 배송 ID: {}", savedDelivery.getOrderId(),
             savedDelivery.getDeliveryId());
 
-        sendDeliveryCreatedMessage(savedDelivery);
 //        sendSlackMessageToDeliveryManager();
     }
 
@@ -69,14 +59,6 @@ public class DeliveryServiceImpl implements DeliveryService {
         // 배송 --- 출발 허브 ID, 목적지 허브 ID ---> 사용자
         // 사용자 --- 출발 허브 배송 담당자 ID, 목적지 허브 업체 배송 담당자 ID ---> 배송
         return new DeliveryManagers();
-    }
-
-    private void sendDeliveryCreatedMessage(Delivery savedDelivery) {
-        DeliveryCreatedCompletedMessage message = DeliveryCreatedCompletedMessage.builder()
-            .orderId(savedDelivery.getOrderId())
-            .deliveryId(savedDelivery.getDeliveryId())
-            .build();
-        rabbitTemplate.convertAndSend(queueCreatedCompleted, message);
     }
 
     private void sendSlackMessageToDeliveryManager() {

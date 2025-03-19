d75da921515c28a623250b1bbb673010ed9d9b09
khloe08 <owlnae04@gmail.com>
2025-03-19 16:09:41 +0900

    [feat #57] User 수정사항

--- user/src/main/java/com/msa/fiveio/user/application/UsersService.java
+++ user/src/main/java/com/msa/fiveio/user/application/UsersService.java
@@ -6,6 +6,7 @@ import com.msa.fiveio.user.model.entity.Users;
 import com.msa.fiveio.user.model.entity.UsersFactory;
 import com.msa.fiveio.user.model.entity.enums.UsersRoleEnum;
 import com.msa.fiveio.user.model.repository.UsersRepository;
+import com.msa.fiveio.user.presentation.dto.UserDto;
 import com.msa.fiveio.user.presentation.dto.UsersSignUpRequestDto;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -16,45 +17,41 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UsersService {
 
-  private final UsersRepository usersRepository;
-  private final PasswordEncoder passwordEncoder;
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
 
-  //회원가입
-  public void signup(UsersSignUpRequestDto usersSignUpRequestDto) {
+    //회원가입
+    public UserDto signup(UsersSignUpRequestDto usersSignUpRequestDto) {
 
-    // 중복 체크
-    usersRepository.findByUsername(usersSignUpRequestDto.getUsername())
-        .ifPresent(user -> {
-          throw new CustomException(UserErrorCode.USER_ERROR_CODE);
-        });
+        // 중복 체크
+        usersRepository.findByUsername(usersSignUpRequestDto.getUsername())
+            .ifPresent(user -> {
+                throw new CustomException(UserErrorCode.USER_ERROR_CODE);
+            });
 
-    // 입력된 role을 Enum으로 변환
-    UsersRoleEnum roleEnum;
-    try {
-      roleEnum = UsersRoleEnum.valueOf(usersSignUpRequestDto.getRole());
-    } catch (IllegalArgumentException e) {
-      throw new CustomException(UserErrorCode.ENUM_ERROR_CODE);
+        // 입력된 role을 Enum으로 변환
+        UsersRoleEnum roleEnum = usersSignUpRequestDto.getRole();
+
+        // 빌더형태로 저장 , 비밀번호 암호화
+        Users user = UsersFactory.createUser(
+            usersSignUpRequestDto.getUsername(),
+            passwordEncoder.encode(usersSignUpRequestDto.getPassword()),
+            usersSignUpRequestDto.getSlackId(),
+            roleEnum
+        );
+
+        usersRepository.save(user);
+
+        return UserDto.of(user);
     }
 
-    // 빌더형태로 저장 , 비밀번호 암호화
-    Users user = UsersFactory.createUser(
-        usersSignUpRequestDto.getUsername(),
-        passwordEncoder.encode(usersSignUpRequestDto.getPassword()),
-        usersSignUpRequestDto.getSlackId(),
-        usersSignUpRequestDto.getEmail(),
-        roleEnum
-    );
+    //로그아웃
+    public void logout(String token) {
+    }
 
-    usersRepository.save(user);
-  }
-
-  //로그아웃
-  public void logout(String token) {
-  }
-
-  //사용자 정보 조회
-  public Optional<Users> getUserInfo(String username) {
-    return usersRepository.findByUsername(username);
-  }
+    //사용자 정보 조회
+    public Optional<Users> getUserInfo(String username) {
+        return usersRepository.findByUsername(username);
+    }
 }
--- user/src/main/java/com/msa/fiveio/user/presentation/controller/UsersController.java
+++ user/src/main/java/com/msa/fiveio/user/presentation/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.msa.fiveio.user.presentation.controller;
 
 import com.msa.fiveio.user.application.UsersService;
 import com.msa.fiveio.user.model.repository.UsersRepository;
+import com.msa.fiveio.user.presentation.dto.UserDto;
 import com.msa.fiveio.user.presentation.dto.UsersSignUpRequestDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -19,21 +20,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UsersController {
 
-  private final UsersService usersService;
-  private final UsersRepository usersRepository;
+    private final UsersService usersService;
+    private final UsersRepository usersRepository;
 
 //    @Operation(summary = "사용자 로그아웃", description = "사용자 로그아웃 후 세션을 만료")
 //    @PostMapping("/logout")
 //    public ResponseEntity<Users> logout {
 //    }
 
-  @PostMapping("/signup")
-  @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
-  public ResponseEntity<String> signup(
-      @Valid @RequestBody UsersSignUpRequestDto usersSignUpRequestDto) {
-    usersService.signup(usersSignUpRequestDto);
-    return ResponseEntity.ok("회원가입 성공");
-  }
+    @PostMapping("/signUp")
+    @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
+    public ResponseEntity<UserDto> signup(
+        @Valid @RequestBody UsersSignUpRequestDto usersSignUpRequestDto) {
+        UserDto userDto = usersService.signup(usersSignUpRequestDto);
+        return ResponseEntity.ok(userDto);
+    }
 
 //    @PostMapping("/api/users")
 //    @Operation(summary = "사용자 정보 조회", description = "사용자의 정보를 확인합니다")

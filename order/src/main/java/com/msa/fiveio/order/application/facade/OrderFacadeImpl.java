16c7ce3914a3b3cdc4d30a75627868719bcd8459
문고은 <>
2025-03-20 00:47:12 +0900

    [feat #84] OrderFacade에서 외부 서비스 연동 로직 추가

--- order/src/main/java/com/msa/fiveio/order/application/facade/OrderFacadeImpl.java
+++ order/src/main/java/com/msa/fiveio/order/application/facade/OrderFacadeImpl.java
@@ -1,10 +1,14 @@
 package com.msa.fiveio.order.application.facade;
 
+import com.msa.fiveio.order.application.usecase.ExternalService;
 import com.msa.fiveio.order.application.usecase.OrderService;
+import com.msa.fiveio.order.infrastructure.client.dto.response.CompanyResponseDto;
+import com.msa.fiveio.order.model.entity.Order;
 import com.msa.fiveio.order.presentation.dto.request.OrderCreateRequestDto;
 import com.msa.fiveio.order.presentation.dto.request.OrderSearchRequestDto;
 import com.msa.fiveio.order.presentation.dto.response.OrderCreateResponseDto;
 import com.msa.fiveio.order.presentation.dto.response.OrderResponseDto;
+import com.msa.fiveio.order.presentation.mapper.OrderMapper;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,10 +20,18 @@ import org.springframework.stereotype.Service;
 public class OrderFacadeImpl implements OrdersFacade {
 
     private final OrderService orderService;
+    private final ExternalService externalService;
 
     @Override
     public OrderCreateResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto) {
-        return orderService.createOrder(orderCreateRequestDto);
+        CompanyResponseDto companyResponseDto = externalService.sendCompanyRequest(
+            orderCreateRequestDto);
+        Order order = orderCreateRequestDto.createOrder(companyResponseDto.getRequesterCompanyId());
+        Order savedOrder = orderService.createOrder(companyResponseDto, order);
+
+        externalService.sendDeliveryRequest(savedOrder.getOrderId(), companyResponseDto,
+            orderCreateRequestDto);
+        return OrderMapper.orderIdToOrderCreateResponseDto(savedOrder);
     }
 
     @Override

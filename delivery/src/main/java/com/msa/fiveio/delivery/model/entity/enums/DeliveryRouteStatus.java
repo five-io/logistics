6834398dccd94abe1fd23834af5a01e0080419fc
khloe08 <owlnae04@gmail.com>
2025-03-20 16:01:07 +0900

    [feat #74] 파라미터 수정

--- delivery/src/main/java/com/msa/fiveio/delivery/model/entity/enums/DeliveryRouteStatus.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/model/entity/enums/DeliveryRouteStatus.java
@@ -1,8 +1,14 @@
 package com.msa.fiveio.delivery.model.entity.enums;
 
 public enum DeliveryRouteStatus {
-    WAITING_AT_HUB,             // 허브 이동 대기중
-    IN_TRANSIT_TO_HUB,          // 허브 이동중
-    ARRIVED_AT_DESTINATION_HUB, // 목적지 허브 도착
-    IN_DELIVERY                 // 배송중
+    WAITING_AT_HUB("허브 이동 대기중"),             // 허브 이동 대기중
+    IN_TRANSIT_TO_HUB("허브 이동중"),          // 허브 이동중
+    ARRIVED_AT_DESTINATION_HUB("목적지 허브 도착"), // 목적지 허브 도착
+    IN_DELIVERY("배송중");                // 배송중
+
+    private final String status;
+
+    DeliveryRouteStatus(String status) {
+        this.status = status;
+    }
 }
--- hub/src/main/java/com/msa/fiveio/hub/application/usecase/HubRouteService.java
+++ hub/src/main/java/com/msa/fiveio/hub/application/usecase/HubRouteService.java
@@ -1,11 +1,10 @@
 package com.msa.fiveio.hub.application.usecase;
 
-import com.msa.fiveio.hub.presentation.dto.hubRoutes.HubRouteRequestDto;
 import com.msa.fiveio.hub.presentation.dto.hubRoutes.HubRouteResponseDto;
 import com.msa.fiveio.hub.presentation.dto.hubs.HubsResponseDto;
 
 public interface HubRouteService {
 
-    HubRouteResponseDto createHubRoute(HubsResponseDto start, HubsResponseDto end,
-        HubRouteRequestDto hubsDto);
+    HubRouteResponseDto createHubRoute(HubsResponseDto start, HubsResponseDto end);
+
 }

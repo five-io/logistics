68cd1321ef57f25443b8ef5ead99f5649919ea4b
adgjl <adgjl0122@naver.com>
2025-03-20 23:38:42 +0900

    [feat #95] slacks CreateDto 수정

--- slack/src/main/java/com/msa/fiveio/slack/presentation/dto/SlacksCreateRequestDto.java
+++ slack/src/main/java/com/msa/fiveio/slack/presentation/dto/SlacksCreateRequestDto.java
@@ -1,7 +1,6 @@
 package com.msa.fiveio.slack.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,17 +9,48 @@ import lombok.Getter;
 @Builder
 public class SlacksCreateRequestDto {
 
-	@JsonProperty("user-id")
-	private Long userId;
-
-	@JsonProperty("receive-id")
-	private UUID receiveId;
+	@JsonProperty("receiver-company-id")
+	private UUID receiverCompanyId;
 
 	@JsonProperty("order-id")
 	private UUID orderId;
 
+	@JsonProperty("depart-id")
+	private UUID departId;
+
+	@JsonProperty("transit-point")
+	private String transitPoint;
+
+	@JsonProperty("arrive-id")
+	private UUID arriveId;
+
+	@JsonProperty("delivery-status")
+	private String deliveryStatus;
+
+	@JsonProperty("delivery-address")
+	private String deliveryAddress;
+
+	@JsonProperty("product-id")
+	private UUID productId;
+
+	@JsonProperty("recipient-name")
+	private String recipientName;
+
+	@JsonProperty("recipient-slack-id")
+	private String recipientSlackId;
+
+	@JsonProperty("company-delivery-manager")
+	private String companyDeliveryManager;
+
+	@JsonProperty("product-name")
+	private String productName;
+
+	@JsonProperty("product-quantity")
+	private String productQuantity;
+
+	@JsonProperty("request-notes")
+	private String requestNotes;
+
 	private String message;
 
-	@JsonProperty("delivery-time")
-	private LocalDateTime deliveryTime;
 }

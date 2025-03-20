235cd7d928be4efa4225245856343530c86b44f0
syn <tlsdpsk1992@naver.c0m>
2025-03-21 03:11:11 +0900

    [feat #67] order 요청 API 생성, CompanyClient 생성

--- product/src/main/java/com/msa/fiveio/product/presentation/controller/ProductsController.java
+++ product/src/main/java/com/msa/fiveio/product/presentation/controller/ProductsController.java
@@ -1,14 +1,18 @@
 package com.msa.fiveio.product.presentation.controller;
 
 import com.msa.fiveio.product.application.facade.ProductFacade;
+import com.msa.fiveio.product.presentation.dto.OrderProductGetResponseDto;
 import com.msa.fiveio.product.presentation.dto.ProductCreateRequestDto;
 import com.msa.fiveio.product.presentation.dto.ProductCreateResponseDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,6 +31,16 @@ public class ProductsController {
         return ResponseEntity.ok(responseDto);
     }
 
+    //order 로부터 정보 받아와서 다시 반환
+    @GetMapping("/order")
+    OrderProductGetResponseDto processOrderRequest(@RequestParam UUID productId,
+            @RequestParam UUID receiverCompanyId, @RequestParam Long quantity) {
+        System.out.println(1_1);
+        OrderProductGetResponseDto responseDto = productFacade.processOrderRequest(productId,
+                receiverCompanyId, quantity);
+        System.out.println(1);
+        return responseDto;
+    }
     //상품조회
 
     //상품수정

95d5764793879450f0703172228bb10b36b49bd1
syn <tlsdpsk1992@naver.c0m>
2025-03-21 03:22:47 +0900

    [feat #67] facade - 주문상품,업체 합침

--- product/src/main/java/com/msa/fiveio/product/application/facade/ProductFacadeImpl.java
+++ product/src/main/java/com/msa/fiveio/product/application/facade/ProductFacadeImpl.java
@@ -1,8 +1,13 @@
 package com.msa.fiveio.product.application.facade;
 
 import com.msa.fiveio.product.application.usecase.ProductService;
+import com.msa.fiveio.product.infrastructure.client.CompanyClient;
+import com.msa.fiveio.product.infrastructure.client.ProductCompanyGetResponseDto;
+import com.msa.fiveio.product.model.entity.Products;
+import com.msa.fiveio.product.presentation.dto.OrderProductGetResponseDto;
 import com.msa.fiveio.product.presentation.dto.ProductCreateRequestDto;
 import com.msa.fiveio.product.presentation.dto.ProductCreateResponseDto;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +16,34 @@ import org.springframework.stereotype.Service;
 public class ProductFacadeImpl implements ProductFacade {
 
     private final ProductService productService;
+    private final CompanyClient companyClient;
 
     @Override
     public ProductCreateResponseDto createProduct(ProductCreateRequestDto productRequestDto) {
         return productService.createProduct(productRequestDto);
+    }
+
+    @Override
+    public OrderProductGetResponseDto processOrderRequest(UUID productId, UUID receiverCompanyId,
+            Long quantity) {
+        System.out.println(2);
+        ProductCompanyGetResponseDto companyDto = companyClient.getProductCompany(
+                receiverCompanyId).getBody();
+        System.out.println(3);
+        Products product = productService.processOrderRequest(productId, receiverCompanyId,
+                quantity);
+        System.out.println(4);
+        OrderProductGetResponseDto responseDto =
+                OrderProductGetResponseDto.builder()
+                        .deliveryAddress(companyDto.getDeliveryAddress())
+                        .requesterCompanyId(product.getCompanyId())
+                        .departHubId(product.getHubId())
+                        .arriveHubId(companyDto.getArriveHubId())
+                        .productPrice(product.getProductPrice())
+                        .build();
+        System.out.println(5);
+
+        return responseDto;
     }
 
 }

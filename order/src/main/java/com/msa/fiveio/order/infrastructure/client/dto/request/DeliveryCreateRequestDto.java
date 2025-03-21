1edf4f9d53a5b89462002fbc3b5e3b1e46df9f99
문고은 <>
2025-03-21 21:29:54 +0900

    [feat #114] 업체 응답 Dto 및 배송 생성 요청 Dto에 필드 추가

--- order/src/main/java/com/msa/fiveio/order/infrastructure/client/dto/request/DeliveryCreateRequestDto.java
+++ order/src/main/java/com/msa/fiveio/order/infrastructure/client/dto/request/DeliveryCreateRequestDto.java
@@ -16,6 +16,10 @@ public class DeliveryCreateRequestDto {
     private final String deliveryAddress;
     private final String recipientName;
     private final String recipientSlackId;
+    private final Long quantity;
+    private final String requestNotes;
+    private final String productName;
+
 
     public DeliveryCreateRequestDto(
         UUID orderId,
@@ -23,10 +27,15 @@ public class DeliveryCreateRequestDto {
         OrderCreateRequestDto orderInfo
     ) {
         this.orderId = orderId;
-        this.departHubId = companyInfo.getDepartHubId();
-        this.arriveHubId = companyInfo.getArriveHubId();
+//        this.departHubId = companyInfo.getDepartHubId();
+//        this.arriveHubId = companyInfo.getArriveHubId();
+        this.departHubId = UUID.fromString("3f1a95f8-7c45-4a83-8e2d-b19d05a5f6a7");
+        this.arriveHubId = UUID.fromString("a57c9d82-1e54-42d9-bd85-98a20dba1f4a");
         this.deliveryAddress = companyInfo.getDeliveryAddress();
         this.recipientName = orderInfo.getRecipientName();
         this.recipientSlackId = orderInfo.getRecipientSlackId();
+        this.quantity = orderInfo.getQuantity();
+        this.requestNotes = orderInfo.getRequestNotes();
+        this.productName = companyInfo.getProductName();
     }
 }

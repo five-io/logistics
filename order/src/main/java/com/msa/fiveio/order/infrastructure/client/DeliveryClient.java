f260a68bb993f500877403b6afc34feb7aebf7b5
문고은 <>
2025-03-23 03:11:56 +0900

    [feat #142] Feign Client name과 url 설정

--- order/src/main/java/com/msa/fiveio/order/infrastructure/client/DeliveryClient.java
+++ order/src/main/java/com/msa/fiveio/order/infrastructure/client/DeliveryClient.java
@@ -9,12 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "localhost:19092", configuration = FeignConfig.class)
+@FeignClient(name = "deliveryClient", url = "localhost:19092", configuration = FeignConfig.class)
 public interface DeliveryClient {
 
     @PostMapping("/api/deliveries")
-    void createDelivery(
-        @RequestBody DeliveryCreateRequestDto deliveryCreateRequestDto);
+    void createDelivery(@RequestBody DeliveryCreateRequestDto deliveryCreateRequestDto);
 
     @GetMapping("/api/deliveries/{id}/status")
     String getDeliveryStatus(@PathVariable("id") UUID orderId);

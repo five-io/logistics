7ea5545ac4be2758ef6a2ef331453bd460432d04
문고은 <>
2025-03-22 19:05:57 +0900

    [feat #128] ExternalService에 경로 및 담당자 조회 기능 추가, 경유지 조회 구현

--- delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/ExternalServiceImpl.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/application/usecase/ExternalServiceImpl.java
@@ -4,7 +4,12 @@ import com.msa.fiveio.delivery.infrastructure.client.HubClient;
 import com.msa.fiveio.delivery.infrastructure.client.SlackClient;
 import com.msa.fiveio.delivery.infrastructure.client.dto.request.SlacksCreateRequestDto;
 import com.msa.fiveio.delivery.infrastructure.client.dto.response.HubsResponseDto;
+import com.msa.fiveio.delivery.infrastructure.client.dto.response.RouteResponseDto;
+import com.msa.fiveio.delivery.infrastructure.client.dto.response.UserResponseDto;
 import com.msa.fiveio.delivery.presentation.dto.request.DeliveryCreateRequestDto;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -16,16 +21,16 @@ public class ExternalServiceImpl implements ExternalService {
 
     private final SlackClient slackClient;
     private final HubClient hubClient;
+//    private final UserClient userClient;
 
     @Override
-    public void sendSlackRequest(DeliveryCreateRequestDto deliveryCreateRequestDto) {
+    public void sendSlackRequest(DeliveryCreateRequestDto deliveryCreateRequestDto,
+        String wayPoints, String companyDeliveryManagerName) {
         try {
-            HubsResponseDto departHub = hubClient.readHubs(
-                    deliveryCreateRequestDto.getDepartHubId())
-                .getBody();
-            HubsResponseDto arriveHub = hubClient.readHubs(
-                    deliveryCreateRequestDto.getArriveHubId())
-                .getBody();
+            HubsResponseDto departHub = getHubResponseDto(
+                deliveryCreateRequestDto.getDepartHubId());
+            HubsResponseDto arriveHub = getHubResponseDto(
+                deliveryCreateRequestDto.getArriveHubId());
             log.info("hub response: departHub = {}, arriveHub = {}", departHub.getHubName(),
                 arriveHub.getHubName());
 
@@ -33,11 +38,44 @@ public class ExternalServiceImpl implements ExternalService {
                 .deliveryCreateRequestDto(deliveryCreateRequestDto)
                 .departHub(departHub)
                 .arriveHub(arriveHub)
+                .wayPoints(wayPoints)
+                .deliveryManagerName(companyDeliveryManagerName)
                 .build();
 
             slackClient.createSlack(request);
         } catch (Exception e) {
             throw new IllegalArgumentException("Unable to send slack request", e);
         }
+    }
+
+    @Override
+    public List<RouteResponseDto> getHubRouteList(UUID arriveHubId, UUID departHubId) {
+//        RouteRequestDto requestDto = RouteRequestDto.builder()
+//            .arriveHubId(arriveHubId)
+//            .departHubId(departHubId)
+//            .build();
+//        return hubClient.getHubRouteList(requestDto);
+        return hubClient.getHubRouteList(arriveHubId, departHubId);
+    }
+
+    @Override
+    public UserResponseDto getDeliveryManager(UUID hubID, String type) {
+//        return userClient.getDeliveryManagerId(hubID, type);
+        return UserResponseDto.builder()
+            .id(1L)
+            .userName("짱구")
+            .build();
+    }
+
+    @Override
+    public String getWayPoints(List<UUID> wayPointIds) {
+        return wayPointIds.stream()
+            .map(this::getHubResponseDto)
+            .map(HubsResponseDto::getHubName)
+            .collect(Collectors.joining(","));
+    }
+
+    private HubsResponseDto getHubResponseDto(UUID hubId) {
+        return hubClient.readHubs(hubId).getBody();
     }
 }

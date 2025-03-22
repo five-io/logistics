73220feb55ce12c4b95e2cd7c34cd532bac17997
문고은 <>
2025-03-22 19:08:20 +0900

    [feat #128] DeliveryFacade에 배송 경로 및 경유지 관련 로직 추가

--- delivery/src/main/java/com/msa/fiveio/delivery/application/facade/DeliveryFacadeImpl.java
+++ delivery/src/main/java/com/msa/fiveio/delivery/application/facade/DeliveryFacadeImpl.java
@@ -1,27 +1,55 @@
 package com.msa.fiveio.delivery.application.facade;
 
+import com.msa.fiveio.delivery.application.usecase.DeliveryRouteService;
 import com.msa.fiveio.delivery.application.usecase.DeliveryService;
 import com.msa.fiveio.delivery.application.usecase.ExternalService;
+import com.msa.fiveio.delivery.infrastructure.client.dto.response.RouteResponseDto;
+import com.msa.fiveio.delivery.infrastructure.client.dto.response.UserResponseDto;
+import com.msa.fiveio.delivery.presentation.dto.request.CreateDeliveryRouteRequest;
 import com.msa.fiveio.delivery.presentation.dto.request.DeliveryCreateRequestDto;
 import com.msa.fiveio.delivery.presentation.dto.request.DeliverySearchRequestDto;
 import com.msa.fiveio.delivery.presentation.dto.response.DeliveryResponseDto;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class DeliveryFacadeImpl implements DeliveryFacade {
 
     private final DeliveryService deliveryService;
     private final ExternalService externalService;
+    private final DeliveryRouteService deliveryRouteService;
 
     @Override
     public void createDelivery(DeliveryCreateRequestDto deliveryRequestDto) {
-        deliveryService.createDelivery(deliveryRequestDto);
-        externalService.sendSlackRequest(deliveryRequestDto);
+        UserResponseDto companyDeliveryManager = externalService.getDeliveryManager(
+            deliveryRequestDto.getArriveHubId(), "COMPANY");
+        UserResponseDto hubDeliveryManager = externalService.getDeliveryManager(
+            deliveryRequestDto.getDepartHubId(), "HUB");
+
+        DeliveryResponseDto deliveryResponseDto = deliveryService.createDelivery(
+            deliveryRequestDto, companyDeliveryManager.getId());
+
+        List<RouteResponseDto> routeResponseDtos = externalService.getHubRouteList(
+            deliveryRequestDto.getArriveHubId(), deliveryRequestDto.getDepartHubId());
+
+        deliveryRouteService.createDelRoute(
+            new CreateDeliveryRouteRequest(deliveryResponseDto, hubDeliveryManager.getId(),
+                routeResponseDtos));
+
+        List<UUID> waypoints = routeResponseDtos.stream()
+            .map(RouteResponseDto::arriveId)
+            .toList();
+        String waypointsString = externalService.getWayPoints(waypoints);
+
+        externalService.sendSlackRequest(deliveryRequestDto, waypointsString,
+            companyDeliveryManager.getUserName());
     }
 
     @Override

0bfdcaf6323f42a8504e78abea3a2991ed78534d
syn <tlsdpsk1992@naver.c0m>
2025-03-23 01:52:50 +0900

    [feat #104] 에러수정(companyClient)

--- product/src/main/java/com/msa/fiveio/product/infrastructure/client/CompanyClient.java
+++ product/src/main/java/com/msa/fiveio/product/infrastructure/client/CompanyClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("localhost:19094", configuration = FeignConfig.class)
+@FeignClient(name = "localhost:19094", configuration = FeignConfig.class)
 public interface CompanyClient {
 
     @GetMapping("/api/companys/{companyId}/products")

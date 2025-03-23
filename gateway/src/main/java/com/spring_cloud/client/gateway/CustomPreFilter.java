ba2046b8fd2cb4def7ac3e6cc8c744c40f4a581b
Taehoon <lth2943@naver.com>
2025-03-23 21:48:00 +0900

    헤더에 userId받는 부분 약간 수정

--- gateway/src/main/java/com/spring_cloud/client/gateway/CustomPreFilter.java
+++ gateway/src/main/java/com/spring_cloud/client/gateway/CustomPreFilter.java
@@ -117,7 +117,7 @@ public class CustomPreFilter implements GlobalFilter, Ordered {
             .build()
             .parseClaimsJws(jwtToken)
             .getBody()
-            .get("user_id", String.class);
+            .get("userId", String.class);
     }
 
     private String getRoleFromToken(String jwtToken) {

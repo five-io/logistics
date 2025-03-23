84b53c812804d369f0b7a14fd02140d83b1f772b
Taehoon <lth2943@naver.com>
2025-03-23 22:44:17 +0900

    [feat/#61] 사용자 조회 추가

--- user/src/main/java/com/msa/fiveio/user/application/UsersService.java
+++ user/src/main/java/com/msa/fiveio/user/application/UsersService.java
@@ -4,11 +4,12 @@ import com.msa.fiveio.common.exception.CustomException;
 import com.msa.fiveio.common.exception.domain.UserErrorCode;
 import com.msa.fiveio.user.model.entity.Users;
 import com.msa.fiveio.user.model.entity.UsersFactory;
+import com.msa.fiveio.user.model.entity.enums.ManagersTypeEnum;
 import com.msa.fiveio.user.model.entity.enums.UsersRoleEnum;
 import com.msa.fiveio.user.model.repository.UsersRepository;
-import com.msa.fiveio.user.presentation.dto.UserDto;
+import com.msa.fiveio.user.presentation.dto.UsersProfileDto;
 import com.msa.fiveio.user.presentation.dto.UsersSignUpRequestDto;
-import java.util.Optional;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -17,41 +18,41 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class UsersService {
 
-    private final UsersRepository usersRepository;
-    private final PasswordEncoder passwordEncoder;
+  private final UsersRepository usersRepository;
+  private final PasswordEncoder passwordEncoder;
 
 
-    //회원가입
-    public UserDto signup(UsersSignUpRequestDto usersSignUpRequestDto) {
+  //회원가입
+  public void signUp(UsersSignUpRequestDto usersSignUpRequestDto) {
+    UsersRoleEnum roleEnum = usersSignUpRequestDto.getRole();
+    ManagersTypeEnum managersType = usersSignUpRequestDto.getManagersType();
 
-        // 중복 체크
-        usersRepository.findByUsername(usersSignUpRequestDto.getUsername())
-            .ifPresent(user -> {
-                throw new CustomException(UserErrorCode.USER_ERROR_CODE);
-            });
+    //유저네임 중복체크
+    usersRepository.findByUsername(usersSignUpRequestDto.getUsername())
+        .ifPresent(user -> {
+          throw new CustomException(UserErrorCode.USER_ALEADY_EXIST);
+        });
 
-        // 입력된 role을 Enum으로 변환
-        UsersRoleEnum roleEnum = usersSignUpRequestDto.getRole();
+    Users users = UsersFactory.createUser(
+        usersSignUpRequestDto.getUsername(),
+        passwordEncoder.encode(usersSignUpRequestDto.getPassword()),
+        usersSignUpRequestDto.getSlackId(),
+        UUID.fromString(usersSignUpRequestDto.getHubId()),
+        roleEnum
+//        managersType
+    );
 
-        // 빌더형태로 저장 , 비밀번호 암호화
-        Users user = UsersFactory.createUser(
-            usersSignUpRequestDto.getUsername(),
-            passwordEncoder.encode(usersSignUpRequestDto.getPassword()),
-            usersSignUpRequestDto.getSlackId(),
-            roleEnum
-        );
+    usersRepository.save(users);
+  }
 
-        usersRepository.save(user);
+  //로그아웃
+  public void logout(String token) {
+  }
 
-        return UserDto.of(user);
-    }
-
-    //로그아웃
-    public void logout(String token) {
-    }
-
-    //사용자 정보 조회
-    public Optional<Users> getUserInfo(String username) {
-        return usersRepository.findByUsername(username);
-    }
+  //사용자 정보 조회
+  public UsersProfileDto getUserProfile(Long userId) {
+    Users users = usersRepository.findByUserId(userId)
+        .orElseThrow(() -> new CustomException(UserErrorCode.USER_ERROR_CODE));
+    return new UsersProfileDto(users);
+  }
 }

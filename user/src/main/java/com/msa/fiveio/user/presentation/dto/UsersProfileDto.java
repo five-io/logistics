26d9cdb446e681e73d36fd928dc93af2c569d74a
Taehoon <lth2943@naver.com>
2025-03-24 11:40:55 +0900

    [feat/#91] 사용자 조회 dto 수정

--- user/src/main/java/com/msa/fiveio/user/presentation/dto/UsersProfileDto.java
+++ user/src/main/java/com/msa/fiveio/user/presentation/dto/UsersProfileDto.java
@@ -15,13 +15,16 @@ public class UsersProfileDto {
 
   private String hubId;
 
+  private String role;
+
   private LocalDateTime createDate;
 
   public UsersProfileDto(Users users) {
-    this.username = users.getUsername();
     this.userId = users.getUserId();
+    this.username = users.getUsername();
     this.slackId = users.getSlackId();
     this.hubId = String.valueOf(users.getHubId());
+    this.role= String.valueOf(users.getRole());
     this.createDate = users.getCreatedAt();
   }
 }
